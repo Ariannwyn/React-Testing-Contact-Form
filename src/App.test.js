0c4bb0c578b09403d3d91142855d492d3cbc159a
EmilyA <emilyadams.3d@gmail.com>
2020-09-03 11:10:01 -0700

    Test mostly works

--- src/App.test.js
+++ src/App.test.js
@@ -11,35 +11,34 @@ test("contact form creates a new contact", () => {
   const { getByLabelText, getByText, findAllByText } = render(<ContactForm />);
 
   //query for all inputs
-  const firstNameInput = getByLabelText("First Name");
-
-  // const lastNameInput = screen.getByLabelText(/lastName/i);
-  // const companyInput = screen.getByLabelText(/companyName/i);
-  // const emailInput = screen.getByLabelText(/email/i);
-  // const messageInput = screen.getByLabelText(/message/i);
+  const firstNameInput = getByLabelText(/First Name*/i);
+  const lastNameInput = getByLabelText(/Last Name*/i);
+  const companyInput = getByLabelText(/Company Name*/i);
+  const emailInput = getByLabelText(/Email*/i);
+  const messageInput = getByLabelText(/Message/i);
 
   //function to fill in the inputs
-  // fireEvent.change(firstNameInput, {
-  //   target: { name: "firstName", value: "Emily" },
-  // });
-  // fireEvent.change(lastNameInput, {
-  //   target: { name: "lastName", value: "Adams" },
-  // });
-  // fireEvent.change(companyInput, {
-  //   target: { name: "companyName", value: "Microsoft" },
-  // });
-  // fireEvent.change(emailInput, {
-  //   target: { name: "email", value: "email@email.com" },
-  // });
-  // fireEvent.change(messageInput, {
-  //   target: { name: "message", value: "Hi" },
-  // });
+  fireEvent.change(firstNameInput, {
+    target: { name: "firstName", value: "Emily" },
+  });
+  fireEvent.change(lastNameInput, {
+    target: { name: "lastName", value: "Adams" },
+  });
+  fireEvent.change(companyInput, {
+    target: { name: "companyName", value: "Microsoft" },
+  });
+  fireEvent.change(emailInput, {
+    target: { name: "email", value: "email@email.com" },
+  });
+  fireEvent.change(messageInput, {
+    target: { name: "message", value: "Hi" },
+  });
 
   //Click submit
-  // const submitButton = screen.getByType(/submit/i); //is getByType a thing?
-  // fireEvent.click(submitButton);
+  const submitButton = screen.getByTestId(/submit/i); //is getByType a thing?
+  fireEvent.click(submitButton);
 
   //Results
-  //   const newFirstName = screen.getByText(/emily/i)
-  //   expect(newFirstName).toBeInTheDocument();
+  // const newFirstName = screen.getByText(/emily/i);
+  // expect(newFirstName).toBeInTheDocument();
 });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,8 +14,9 @@ const ContactForm = () => {
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="firstName">First Name</label>
+          <label htmlFor="firstName">First Name*</label>
           <input
+            id="firstName"
             name="firstName"
             placeholder="Edd"
             ref={register({ required: true, maxLength: 3 })}
@@ -28,6 +29,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id="lastName"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -39,6 +41,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="companyName">Company Name*</label>
           <input
+            id="companyName"
             name="companyName"
             placeholder="Apple"
             ref={register({ required: true })}
@@ -52,21 +55,25 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id="email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea
+            id="message"
+            name="message"
+            ref={register({ required: false })}
+          />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input type="submit" data-testid="submit" />
       </form>
     </div>
   );
